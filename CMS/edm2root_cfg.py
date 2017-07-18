46d2dc96d8db6393ec20addf67c11bf5f7b4baec
Thomas Mueller <t.muller@cern.ch>
2017-07-18 18:05:18 +0200

    added testing file from http://opendata.cern.ch/record/17

--- CMS/edm2root_cfg.py
+++ CMS/edm2root_cfg.py
@@ -4,12 +4,12 @@ process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        'file:myfile.root'
+        'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/DoubleMu/AOD/12Oct2013-v1/10000/000D143E-9535-E311-B88B-002618943934.root'
     )
 )
 
