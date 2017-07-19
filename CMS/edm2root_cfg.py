8cc45e77a223ea88b506bce463ed84366b3ef2ce
Thomas Mueller <t.muller@cern.ch>
2017-07-19 16:21:59 +0200

    edm2root_cfg.py: added HLT filter

--- CMS/edm2root_cfg.py
+++ CMS/edm2root_cfg.py
@@ -7,10 +7,14 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
-    # replace 'myfile.root' with the source file you want to use
-    fileNames = cms.untracked.vstring(
-        'root://eospublic.cern.ch//eos/opendata/cms/Run2011A/DoubleMu/AOD/12Oct2013-v1/10000/000D143E-9535-E311-B88B-002618943934.root'
-    )
+	# replace "myfile.root" with the source file you want to use
+	fileNames = cms.untracked.vstring(
+		# http://opendata.cern.ch/record/32
+		#"root://eospublic.cern.ch//eos/opendata/cms/Run2011A/SingleMu/AOD/12Oct2013-v1/10000/00082EAF-C03D-E311-8E53-003048F00B1C.root",
+		
+		# http://opendata.cern.ch/record/17
+		"root://eospublic.cern.ch//eos/opendata/cms/Run2011A/DoubleMu/AOD/12Oct2013-v1/10000/000D143E-9535-E311-B88B-002618943934.root",
+	)
 )
 
 process.TFileService = cms.Service("TFileService",
@@ -18,7 +22,17 @@ process.TFileService = cms.Service("TFileService",
 	closeFileFast = cms.untracked.bool(True)
 )
 
-process.events = cms.EDAnalyzer('EDM2ROOT'
+# https://twiki.cern.ch/twiki/bin/viewauth/CMS/TriggerResultsFilter#Use_for_Offline_analysis
+# https://twiki.cern.ch/twiki/bin/view/Main/AndreHolznerCMSSW#How_do_I_select_events_which_pas
+import HLTrigger.HLTfilters.triggerResultsFilter_cfi as hlt
+process.triggerResultsFilter = hlt.triggerResultsFilter.clone(
+	hltResults = cms.InputTag("TriggerResults","","HLT"),
+	triggerConditions = cms.vstring("HLT_Mu17_Mu8_v*", ),
+	l1tResults = "",
+	throw = cms.bool(True)
 )
 
-process.p = cms.Path(process.events)
+process.events = cms.EDAnalyzer("EDM2ROOT"
+)
+
+process.p = cms.Path(process.triggerResultsFilter + process.events)
