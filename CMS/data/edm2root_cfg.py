d4f1a48ecabd39c637619336729a6bb40c091900
Thomas Mueller <t.muller@cern.ch>
2017-07-20 15:05:13 +0200

    edm2root_cfg.py: added config options

--- CMS/data/edm2root_cfg.py
+++ CMS/data/edm2root_cfg.py
@@ -1,5 +1,17 @@
 import FWCore.ParameterSet.Config as cms
 
+# http://opendata.cern.ch/record/32
+#test_inputs = ["root://eospublic.cern.ch//eos/opendata/cms/Run2011A/SingleMu/AOD/12Oct2013-v1/10000/00082EAF-C03D-E311-8E53-003048F00B1C.root"]
+
+# http://opendata.cern.ch/record/17
+test_inputs = ["root://eospublic.cern.ch//eos/opendata/cms/Run2011A/DoubleMu/AOD/12Oct2013-v1/10000/000D143E-9535-E311-B88B-002618943934.root"]
+
+from FWCore.ParameterSet.VarParsing import VarParsing
+options = VarParsing("python")
+options.register("input", test_inputs, VarParsing.multiplicity.list, VarParsing.varType.string, "Input files")
+options.register("output", "output.root", VarParsing.multiplicity.singleton, VarParsing.varType.string, "Output file")
+options.parseArguments()
+
 process = cms.Process("EDM2ROOT")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
@@ -8,17 +20,11 @@ process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 process.source = cms.Source("PoolSource",
 	# replace "myfile.root" with the source file you want to use
-	fileNames = cms.untracked.vstring(
-		# http://opendata.cern.ch/record/32
-		#"root://eospublic.cern.ch//eos/opendata/cms/Run2011A/SingleMu/AOD/12Oct2013-v1/10000/00082EAF-C03D-E311-8E53-003048F00B1C.root",
-		
-		# http://opendata.cern.ch/record/17
-		"root://eospublic.cern.ch//eos/opendata/cms/Run2011A/DoubleMu/AOD/12Oct2013-v1/10000/000D143E-9535-E311-B88B-002618943934.root",
-	)
+	fileNames = cms.untracked.vstring(*options.input)
 )
 
 process.TFileService = cms.Service("TFileService",
-	fileName = cms.string("output.root"),
+	fileName = cms.string(options.output),
 	closeFileFast = cms.untracked.bool(True)
 )
 
